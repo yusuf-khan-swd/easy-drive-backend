09ed1217489f72935e7153b6dd09cd1908348fc4
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-07-17 14:53:21 +0600

    update auth validation of email

--- src/app/modules/Auth/auth.validation.ts
+++ src/app/modules/Auth/auth.validation.ts
@@ -2,7 +2,7 @@ import { z } from 'zod';
 
 const loginValidationSchema = z.object({
   body: z.object({
-    id: z.string({ required_error: 'Id is required' }),
+    email: z.string({ required_error: 'Id is required' }),
     password: z.string({ required_error: 'Password is required' }),
   }),
 });
