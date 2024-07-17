fec9f659453e12e87b4aac785779b8a9285f6b70
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-07-17 19:42:01 +0600

    remove unused  changePasswordValidationSchema,   refreshTokenValidationSchema from auth schema

--- src/app/modules/Auth/auth.validation.ts
+++ src/app/modules/Auth/auth.validation.ts
@@ -7,23 +7,6 @@ const loginValidationSchema = z.object({
   }),
 });
 
-const changePasswordValidationSchema = z.object({
-  body: z.object({
-    oldPassword: z.string({ required_error: 'Old Password is required' }),
-    newPassword: z.string({ required_error: 'New Password is required' }),
-  }),
-});
-
-const refreshTokenValidationSchema = z.object({
-  cookies: z.object({
-    refreshToken: z.string({
-      required_error: 'Refresh token is required!',
-    }),
-  }),
-});
-
 export const AuthValidation = {
   loginValidationSchema,
-  changePasswordValidationSchema,
-  refreshTokenValidationSchema,
 };
