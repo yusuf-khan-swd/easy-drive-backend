179adf12afee3337fda41a5ab50cda69db4cb62c
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-07-17 19:18:40 +0600

    rename variable errorSources to errorMessages and remove err from response json in globalErrorHandler

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -13,7 +13,7 @@ import { TErrorSources } from '../interface/error';
 const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
   let statusCode = 500;
   let message = 'Something went wrong';
-  let errorSources: TErrorSources = [
+  let errorMessages: TErrorSources = [
     { path: '', message: 'Something went wrong' },
   ];
 
@@ -21,36 +21,35 @@ const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
     const simplifiedError = handleZodError(err);
     statusCode = simplifiedError?.statusCode;
     message = simplifiedError?.message;
-    errorSources = simplifiedError?.errorSources;
+    errorMessages = simplifiedError?.errorSources;
   } else if (err?.name === 'ValidationError') {
     const simplifiedError = handleValidationError(err);
     statusCode = simplifiedError?.statusCode;
     message = simplifiedError?.message;
-    errorSources = simplifiedError?.errorSources;
+    errorMessages = simplifiedError?.errorSources;
   } else if (err?.name === 'CastError') {
     const simplifiedError = handleCastError(err);
     statusCode = simplifiedError?.statusCode;
     message = simplifiedError?.message;
-    errorSources = simplifiedError?.errorSources;
+    errorMessages = simplifiedError?.errorSources;
   } else if (err?.code === 11000) {
     const simplifiedError = handleDuplicateError(err);
     statusCode = simplifiedError?.statusCode;
     message = simplifiedError?.message;
-    errorSources = simplifiedError?.errorSources;
+    errorMessages = simplifiedError?.errorSources;
   } else if (err instanceof AppError) {
     statusCode = err?.statusCode;
     message = err?.message;
-    errorSources = [{ path: '', message: err?.message }];
+    errorMessages = [{ path: '', message: err?.message }];
   } else if (err instanceof Error) {
     message = err?.message;
-    errorSources = [{ path: '', message: err?.message }];
+    errorMessages = [{ path: '', message: err?.message }];
   }
 
   return res.status(statusCode).json({
     success: false,
     message,
-    errorSources,
-    err,
+    errorSources: errorMessages,
     stack: config.NODE_ENV === 'development' ? err?.stack : null,
   });
 };
