9e653d148540f126711a2edb2873bc5353a261f2
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-07-17 19:44:36 +0600

    remove unnecessary roles from USER_ROLE

--- src/app/modules/User/user.constant.ts
+++ src/app/modules/User/user.constant.ts
@@ -1,9 +1,4 @@
 export const USER_ROLE = {
-  superAdmin: 'superAdmin',
-  student: 'student',
-  faculty: 'faculty',
   user: 'user',
   admin: 'admin',
 } as const;
-
-export const UserStatus = ['in-progress', 'blocked'];
