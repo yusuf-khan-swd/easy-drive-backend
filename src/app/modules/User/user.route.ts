5f31ffe432d73b4d5e8e1f7afec4c82be618f05e
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-08-29 13:36:06 +0600

    add route for  createAdmin,   getAllUsers,

---
+++ src/app/modules/User/user.route.ts
@@ -0,0 +1,19 @@
+import express from 'express';
+import auth from '../../middlewares/auth';
+import validateRequest from '../../middlewares/validateRequest';
+import { USER_ROLE } from './user.constant';
+import { UserController } from './user.controller';
+import { UserValidation } from './user.validation';
+
+const router = express.Router();
+
+router.post(
+  '/admin',
+  auth(USER_ROLE.admin),
+  validateRequest(UserValidation.userValidationSchema),
+  UserController.createAdmin,
+);
+
+router.get('/', auth(USER_ROLE.admin), UserController.getAllUsers);
+
+export const UserRoutes = router;
