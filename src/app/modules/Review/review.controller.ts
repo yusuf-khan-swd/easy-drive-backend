c02f5e2c5635d3c0f2ef21decf056307214f350e
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-09-08 23:39:46 +0600

    remove convert rating

--- src/app/modules/Review/review.controller.ts
+++ src/app/modules/Review/review.controller.ts
@@ -6,8 +6,6 @@ import { ReviewService } from './review.service';
 const createReview = catchAsync(async (req, res) => {
   const data = req.body;
   const userId = req.user?.userId;
-
-  data.rating = Number(data?.rating);
   data.user = userId;
 
   const result = await ReviewService.createReview(data);
