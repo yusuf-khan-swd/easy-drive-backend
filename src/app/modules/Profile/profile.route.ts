09791aebb049eb2b97b51e3a6f122015e1f13b3c
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-09-09 10:27:18 +0600

    Add route for getUserProfile, updateUserProfile

---
+++ src/app/modules/Profile/profile.route.ts
@@ -0,0 +1,23 @@
+import express from 'express';
+import auth from '../../middlewares/auth';
+import validateRequest from '../../middlewares/validateRequest';
+import { USER_ROLE } from '../User/user.constant';
+import { ProfileController } from './profile.controller';
+import { ProfileValidation } from './profile.validation';
+
+const router = express.Router();
+
+router.get(
+  '/:id',
+  auth(USER_ROLE.user, USER_ROLE.admin),
+  ProfileController.getUserProfile,
+);
+
+router.put(
+  '/:id',
+  auth(USER_ROLE.user, USER_ROLE.admin),
+  validateRequest(ProfileValidation.profileUpdateValidationSchema),
+  ProfileController.updateUserProfile,
+);
+
+export const ProfileRoutes = router;
