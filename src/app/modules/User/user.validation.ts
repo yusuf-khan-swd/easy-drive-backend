39a24a7cc36f48866cb4fc40d0899656de8d10c2
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-09-12 18:39:43 +0600

    Change Password Validation Min Length to 6

--- src/app/modules/User/user.validation.ts
+++ src/app/modules/User/user.validation.ts
@@ -4,7 +4,7 @@ const userValidationSchema = z.object({
   body: z.object({
     name: z.string({ required_error: 'Name is required' }),
     email: z.string({ required_error: 'Email is required' }).email(),
-    password: z.string().min(8, 'Password must be at least 8 characters long'),
+    password: z.string().min(6, 'Password must be at least 6 characters long'),
     phone: z.string().optional(),
     address: z.string().optional(),
   }),
