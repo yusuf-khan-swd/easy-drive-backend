1ffa07f1b9a6775f61c73914a27b475c495b127b
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-11-09 10:52:56 +0600

    send api status code via statusCode property for error

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -48,6 +48,7 @@ const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
 
   return res.status(statusCode).json({
     success: false,
+    statusCode,
     message,
     errorMessages,
     stack: config.NODE_ENV === 'development' ? err?.stack : null,
