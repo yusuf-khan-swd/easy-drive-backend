1e86a4f55df16f9cb60266d4777722e0f1c72dbd
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-12-17 10:17:20 +0600

    in both getMyReviews and getSingleReviews getting token from axios client

--- src/app/modules/Review/review.controller.ts
+++ src/app/modules/Review/review.controller.ts
@@ -39,9 +39,6 @@ const getAllReviews = catchAsync(async (req, res) => {
 });
 
 const getMyReviews = catchAsync(async (req, res) => {
-  const token = req.headers.authorization;
-  console.log('getMyReviews ', token);
-
   const { userId } = req.user;
   const result = await ReviewService.getMyReviews(userId, req.query);
 
@@ -66,9 +63,6 @@ const getCarReviews = catchAsync(async (req, res) => {
 });
 
 const getSingleReview = catchAsync(async (req, res) => {
-  const token = req.headers.authorization;
-  console.log('getSingleReview ', token);
-
   const { id } = req.params;
   const result = await ReviewService.getSingleReview(id);
 
