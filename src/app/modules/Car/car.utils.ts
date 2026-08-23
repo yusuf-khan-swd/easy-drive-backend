38c2c2eefd08c9859163e45c27341cd899fde091
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2026-08-23 21:47:49 +0600

    refactor calculateTotalCoast function for clarity in variable naming

--- src/app/modules/Car/car.utils.ts
+++ src/app/modules/Car/car.utils.ts
@@ -3,13 +3,13 @@ export const calculateTotalCoast = (
   endTime: string,
   pricePerHour: number,
 ) => {
-  const startTimeSplit = startTime.split(':');
-  const endTimeSplit = endTime.split(':');
+  const startTimeString = startTime.split(':');
+  const endTimeString = endTime.split(':');
 
-  const startHours = parseInt(startTimeSplit[0]);
-  const startMinutes = parseInt(startTimeSplit[1]);
-  const endHours = parseInt(endTimeSplit[0]);
-  const endMinutes = parseInt(endTimeSplit[1]);
+  const startHours = parseInt(startTimeString[0]);
+  const startMinutes = parseInt(startTimeString[1]);
+  const endHours = parseInt(endTimeString[0]);
+  const endMinutes = parseInt(endTimeString[1]);
 
   const startTotalMinutes = startHours * 60 + startMinutes;
   const endTotalMinutes = endHours * 60 + endMinutes;
